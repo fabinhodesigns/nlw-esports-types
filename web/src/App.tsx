816532169e30ac53113efb8f504c303155f3ce13
fabinho070 <freitasfabiano08@gmail.com>
2022-09-21 18:59:57 -0300

    Add new props css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import './styles/main.css';
+import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
-      <label>testetesteteste</label>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src ={logoImg} alt="" />
     </div>
   )
 }
