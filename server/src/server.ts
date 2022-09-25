cb7894bcc4f630f5c7f6ef2233e883258f0ae708
fabinho070 <freitasfabiano08@gmail.com>
2022-09-25 12:36:31 -0300

    Add Prisma and finished database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,30 @@
 import express from 'express'
 const app = express();
 
-app.get('/ads', (request, response) => {
-    return response.json([
+app.get('/games', (req, res) => {
+    return res.json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+    return res.json([
         {id: 1, name: 'kitty'},
         {id: 2, name: 'kitty2'},
         {id: 3, name: 'kitty3'},
         {id: 4, name: 'kitty4'},
         {id: 5, name: 'kitty5'},
     ])
+});
+
+app.get('/games/:id/discord', (req, res) => {
+    return res.json([])
+});
+
+app.post('/ads', (req, res) => {
+    return res.json([]);
+});
+
+app.post('/createdAt', (req, res) => {
+    return res.json([]);
 });
 
 app.listen(3333);
